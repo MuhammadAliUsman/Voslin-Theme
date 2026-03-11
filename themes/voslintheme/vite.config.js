1af832fba81ed468e7b1269c474fac64e47218aa
MuhammadAliUsman <ali.dua.afan@gmail.com>
2026-03-11 22:40:07 +0500

    Update vite.config.js

--- themes/voslintheme/vite.config.js
+++ themes/voslintheme/vite.config.js
@@ -1,35 +1,36 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import path from "path";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                "themes/voslintheme/css/app.css",
-                "themes/voslintheme/js/app.js"
-            ],
-            buildDirectory: "voslintheme/",
-        }),
-        {
-            name: "blade",
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith(".blade.php")) {
-                    server.ws.send({
-                        type: "full-reload",
-                        path: "*",
-                    });
-                }
-            },
-        },
-    ],
-    css: {
-        postcss: {
-            plugins: [
-                require("tailwindcss")({
-                    config: path.resolve(__dirname, "tailwind.config.js"),
-                }),
-            ],
-        },
+  plugins: [
+    laravel({
+      input: [
+        "themes/voslintheme/css/app.css",
+        "themes/voslintheme/js/app.js"
+      ],
+      buildDirectory: "voslintheme/",
+    }),
+    {
+      name: "blade",
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith(".blade.php")) {
+          server.ws.send({
+            type: "full-reload",
+            path: "*",
+          });
+        }
+      },
     },
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss({
+          config: path.resolve("./themes/voslintheme/tailwind.config.js"),
+        }),
+      ],
+    },
+  },
 });
